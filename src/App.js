16cfa2704a6ac2f9be69f1ae78a6c5e1d84ef09c
Maggie Hedrick <Maggie.Hedrick@inin.com>
2017-01-27 16:06:39 -0500

    move font around

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Raleway" rel="stylesheet" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2 className="header">Welcome to React</h2>

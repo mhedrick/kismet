388f10541c40804ee8613a198d1824233b8922a4
Maggie Hedrick <Maggie.Hedrick@inin.com>
2017-01-27 15:43:09 -0500

    add in gh publish commands

--- src/App.js
+++ src/App.js
@@ -6,12 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <link href="https://fonts.googleapis.com/css?family=Luckiest+Guy|Raleway" rel="stylesheet" />
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2 className="header">Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Hello world!
         </p>
       </div>
     );

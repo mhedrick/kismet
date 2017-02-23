faf4c2cc9b968493a84f1981ca1ba90dfc37c76b
Maggie Hedrick <Maggie.Hedrick@inin.com>
2017-02-23 15:32:59 -0500

    update adorable component to use state and props

--- src/Adorable.js
+++ src/Adorable.js
@@ -4,7 +4,9 @@ class Adorable extends Component {
     render() {
         return (
             <div className="col-md d-flex justify-content-center">
-                <img className="rounded" height="200" width="200" role="presentation" src={"https://api.adorable.io/avatars/face/eyes1/nose2/mouth1/" + this.props.color.slice(1)} />
+                <img className="rounded" 
+                height="200" width="200" role="presentation" 
+                src={`https://api.adorable.io/avatars/face/${this.props.eyes}/${this.props.nose}/${this.props.mouth}/${this.props.color.slice(1)}`} />
             </div>
         );
     }
